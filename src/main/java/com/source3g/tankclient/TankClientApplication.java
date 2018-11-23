541f346ae7f2d7216ee759d8bf460084b86bb8bc
mihuajun <kobe96688@126.com>
2018-11-23 21:04:38 +0800

    init commit

---
+++ src/main/java/com/source3g/tankclient/TankClientApplication.java
@@ -0,0 +1,12 @@
+package com.source3g.tankclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TankClientApplication.class, args);
+	}
+}
